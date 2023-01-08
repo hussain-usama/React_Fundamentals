feadfef9969270a8cc349b42b0bf0b88c27d0ed1
Usama Hussain <usamaahussain23@gmail.com>
2023-01-08 16:56:47 +0500

    formik

--- src/pages/Forms/AddPost.js
+++ src/pages/Forms/AddPost.js
@@ -1,34 +1,55 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useFormik } from "formik";
-const INITTIAL_STATE = {
-  title: '',
-  desc: '',
+
+const initialValues={
+  title:"",
+  desc:"",
+  email:""
+}
+const onSubmit=formData=>{
+  console.log("avlues",formData)
+}
+
+const validate=formData=>{
+  let error={}
+  if(!formData.title){
+    error.title="Required"
+  }
+  if(!formData.desc){
+    error.desc="Required"
+  }
+  if(!formData.email){
+    error.email="Required"
+  }
+  // else if(){
+  //   error.email="Innvalid email"
+  // }
 }
 function AddPost() {
 
-  const [formData, setFormData] = useState({ ...INITTIAL_STATE })
-  const formik=useFormik({})
+  const formik=useFormik({
+    initialValues,
+    onSubmit,
+    validate
+  })
   
-  const handleChange = (e) => {
-    try {
 
-    } catch (error) {
-
-    }
-  }
 
   return (
     <div className="">
+      <form onSubmit={formik.submitForm}>
       <h1>ADD POST HERE</h1>
-      <form>
         <label>TITLE</label><br />
-        <input type={"text"} className="inputField" value={formData.title} onChange={handleChange} />
+        <input type={"text"} className="inputField" name="title" value={formik.values.title} onChange={formik.handleChange} />
         <br />
         <label>DESCRIPTION</label><br />
-        <input type={"text"} className="inputField" value={formData.desc} onChange={handleChange} />
+        <input type={"text"} className="inputField" name="desc" value={formik.values.desc} onChange={formik.handleChange} />
         <br />
-        <button style={{ marginTop: '2rem' }}>SUBMIT</button>
+        <label>EMAIL</label><br />
+        <input type={"text"} className="inputField" name="email" value={formik.values.email} onChange={formik.handleChange} />
+        <br />
+        <button style={{ marginTop: '2rem' }} >SUBMIT</button>
       </form>
     </div>
   );
