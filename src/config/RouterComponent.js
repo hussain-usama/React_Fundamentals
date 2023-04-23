1b958b57604ff371313580884817e9f2e23a6d91
Usama Hussain <usamaahussain23@gmail.com>
2023-04-24 00:53:58 +0500

    Routing structure changed

--- src/config/RouterComponent.js
+++ src/config/RouterComponent.js
@@ -1,44 +1,33 @@
 import React, { lazy, Suspense } from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
 import { PrivateRoutes, PublicRoutes } from "./RouteTypes";
-
-const Login = lazy(() => import("../pages/Login"))
-const Home = lazy(() => import("../pages/Home"))
-const Dashboard = lazy(() => import("../pages/Dashboard"))
-const Post = lazy(() => import("../pages/Post"))
-const AddPost = lazy(() => import("../pages/Forms/AddPost"))
-const PostComments = lazy(() => import("../pages/PostComments"))
+import { routes } from "./routes";
+import NotFound from "./NotFound";
 
 function RouterComp() {
-    let auth = localStorage.getItem("token")
     return (
         <>
             <Suspense>
                 <Routes>
-                    <Route path="/" element={
-                        <PublicRoutes>
-                            <Login />
-                        </PublicRoutes>} />
-                    <Route path="/home" element={
-                        <PrivateRoutes>
-                            <Home />
-                        </PrivateRoutes>} />
-                    <Route path="/dashboard" element={
-                        <PrivateRoutes>
-                            <Dashboard />
-                        </PrivateRoutes>} />
-                    <Route path="/addPost" element={
-                        <PrivateRoutes>
-                            <AddPost />
-                        </PrivateRoutes>} />
-                    <Route path="/posts/:id" element={
-                        <PrivateRoutes>
-                            <Post />
-                        </PrivateRoutes>} />
-                    <Route path="/posts/:id/comments" element={
-                        <PrivateRoutes>
-                            <PostComments />
-                        </PrivateRoutes>} />
+                    {routes.map(({ component: Component, path, public: pub }, index) => {
+                        if (pub) {
+                            return (
+                                <Route path={path} key={index} element={
+                                    <PublicRoutes>
+                                        <Component />
+                                    </PublicRoutes>} />
+                            )
+                        } else {
+                            return (
+                                <Route path={path} key={index} element={
+                                    <PrivateRoutes>
+                                        <Component />
+                                    </PrivateRoutes>} />
+                            )
+                        }
+                    })}
+
+                    <Route path="*"  element={<NotFound/>}  />
                 </Routes>
             </Suspense>
         </>
--- src/config/routes.js
+++ src/config/routes.js
@@ -3,12 +3,22 @@ import React, { lazy } from "react"
 const Login = lazy(() => import("../pages/Login"))
 const Home = lazy(() => import("../pages/Home"))
 const Dashboard = lazy(() => import("../pages/Dashboard"))
+const Post = lazy(() => import("../pages/Post"))
+const AddPost = lazy(() => import("../pages/Forms/AddPost"))
+const PostComments = lazy(() => import("../pages/PostComments"))
+const NotFound = lazy(() => import("./NotFound"))
+const Todos = lazy(() => import("../pages/Todos/Todos"))
 
+/* DEFINING ROUTES */
+/* public false component will only visible if user logged in */
 export const routes = [
     { component: Login, public: true, path: '/' },
     { component: Dashboard, public: false, path: '/dashboard' },
     { component: Home, public: false, path: '/home' },
+    { component: Post, public: false, path: '/posts/:id' },
+    { component: AddPost, public: false, path: '/addPost' },
+    { component: PostComments, public: false, path: '/posts/:id/comments' },
+    { component: Todos, public: false, path: '/todos' },
+    { component: NotFound, public: 'both', path: '*' },
+
 ]
-
-
-/* NOT USING THIS FILE */
