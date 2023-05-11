cc81345f81df0d5976934aaa26973dd6965bbb29
Usama Hussain <usamaahussain23@gmail.com>
2023-05-11 21:26:36 +0500

    promise

--- src/pages/Todos/Todos.js
+++ src/pages/Todos/Todos.js
@@ -1,12 +1,33 @@
-import React, { useContext, useCallback, useState } from "react";
+import React, { useContext, useCallback, useState, useEffect } from "react";
 import { Ctx } from "../../Provider/Provider";
 import TodosChild from "./TodosChild/TodosChild";
+import axios from "axios";
+
+const apiPromise=(url)=>{
+    return new Promise((resolve,reject)=>{
+        axios.get(url)
+        .then(doc=>resolve(doc))
+        .catch(err=>reject(err))
+    })
+}
 
 const Todos=()=>{
 
     // const getContext=useContext(Ctx)
     console.log('PARENT RENDER')
     const [increment,setIncrement]=useState(0)
+
+    useEffect(()=>{
+        (async()=>{
+            try {
+                let setteled=await Promise.all([apiPromise('https://jsonplaceholder.typicode.com/posts'),apiPromise('https://hub.dummyapis.com/delay?seconds=1')])
+                console.log(setteled,'setteled')
+
+            } catch (error) {
+                console.log(error)
+            }
+        })()
+    },[])
 
 
     return(
--- src/pages/Todos/TodosChild/TodosChild.js
+++ src/pages/Todos/TodosChild/TodosChild.js
@@ -1,46 +1,11 @@
 import axios from "axios";
 import React, { memo, useCallback, useEffect, useState } from "react";
 
-const apiPromise=()=>{
-    return new Promise((resolve,reject)=>{
-        axios.get('https://jsonplaceholder.typicode.com/posts')
-        .then(doc=>resolve(doc))
-        .catch(err=>reject(err))
-    })
-}
-
-const apiPromise1=()=>{
-    return new Promise((resolve,reject)=>{
-        setTimeout(() => {
-            axios.get('https://jsonplaceholder.typicode.com/posts')
-            .then(doc=>{
-                // resolve(doc)
-                throw new Error('Error message')
-            })
-            .catch(err=>reject(err))
-        }, 1000);
-    })
-}
-
 const TodosChild=()=>{
     const [todoList,settodoList]=useState([])
     const addFunction=()=>{
         settodoList((prev)=>[...prev, 'new'])
     }
-
-    useEffect(()=>{
-        (async()=>{
-            try {
-                // let[result,result1]=await Promise.all([apiPromise(),apiPromise1()])
-                // console.log(result,result1,'result')
-                let setteled=await Promise.allSettled([apiPromise(),apiPromise1()])
-                console.log(setteled,'setteled')
-
-            } catch (error) {
-                console.log(error)
-            }
-        })()
-    },[])
     return(
         <div>
            <h1>TodosChild 1</h1>
