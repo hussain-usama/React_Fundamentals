af2ead6ce6d50e17f951b99038067e6453e71b7d
Usama Hussain <usamaahussain23@gmail.com>
2023-05-16 17:43:01 +0500

    calling api withh loop

--- src/pages/Todos/Todos.js
+++ src/pages/Todos/Todos.js
@@ -3,40 +3,66 @@ import { Ctx } from "../../Provider/Provider";
 import TodosChild from "./TodosChild/TodosChild";
 import axios from "axios";
 
-const apiPromise=(url)=>{
-    return new Promise((resolve,reject)=>{
-        axios.get(url)
-        .then(doc=>resolve(doc))
-        .catch(err=>reject(err))
-    })
-}
+const apiPromise = (url) => {
+  return new Promise((resolve, reject) => {
+    axios
+      .get(url)
+      .then((doc) => resolve(doc))
+      .catch((err) => reject(err));
+  });
+};
 
-const Todos=()=>{
+const Todos = () => {
+  // const getContext=useContext(Ctx)
+  // console.log('PARENT RENDER')
+  const [increment, setIncrement] = useState(0);
 
-    // const getContext=useContext(Ctx)
-    console.log('PARENT RENDER')
-    const [increment,setIncrement]=useState(0)
+  useEffect(() => {
+    (async () => {
+      try {
+        let promiseAll = await Promise.all([apiPromise("https://jsonplaceholder.typicode.com/posts"),apiPromise("https://hub.dummyapis.com/delay?seconds=1"),]);
+        // console.log(promiseAll, "promiseAll");
+        callApiLoop();
+        callDependentApi();
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, []);
 
-    useEffect(()=>{
-        (async()=>{
-            try {
-                let setteled=await Promise.all([apiPromise('https://jsonplaceholder.typicode.com/posts'),apiPromise('https://hub.dummyapis.com/delay?seconds=1')])
-                console.log(setteled,'setteled')
+  
+  /* call apis with loop */
+  const callApiLoop = async () => {
+    let arrayUrl = ["https://jsonplaceholder.typicode.com/posts","https://jsonplaceholder.typicode.com/posts/1",];
 
-            } catch (error) {
-                console.log(error)
-            }
-        })()
-    },[])
+    /* WITH AND WITHOUT FOR LOOP */
 
+    let responseArray = await Promise.all(arrayUrl.map(async (x) => {
+        let response = await axios.get(x);
+        return response.data;
+      }));
+    console.log(responseArray,'responseArray')
 
-    return(
-        <div>
-           <h1>Parent</h1>
-            <p>Increment : {increment}</p>
-            <button onClick={()=>setIncrement(increment+1)}>+</button>
-            <TodosChild/>
-        </div>
-    )
-}
-export default Todos
+    let responseArray1 = [];
+    for (const element of arrayUrl) {
+      let response = await axios.get(element);
+      responseArray1.push(response);
+    }
+    console.log(responseArray1, "responseArray1");
+  };
+
+  /* call apis having dependency from previous api */
+  const callDependentApi = async () => {
+    let arrayUrl = ["https://jsonplaceholder.typicode.com/posts","https://jsonplaceholder.typicode.com/posts/1"];
+
+  };
+  return (
+    <div>
+      <h1>Parent</h1>
+      <p>Increment : {increment}</p>
+      <button onClick={() => setIncrement(increment + 1)}>+</button>
+      <TodosChild />
+    </div>
+  );
+};
+export default Todos;
