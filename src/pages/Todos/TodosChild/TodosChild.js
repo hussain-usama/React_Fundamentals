6e66c713554e6dffdfe604f68ea789d5ec24ae1c
Usama Hussain <usamaahussain23@gmail.com>
2023-05-17 13:41:04 +0500

    print in pattern

--- src/pages/Todos/TodosChild/TodosChild.js
+++ src/pages/Todos/TodosChild/TodosChild.js
@@ -6,6 +6,24 @@ const TodosChild=()=>{
     const addFunction=()=>{
         settodoList((prev)=>[...prev, 'new'])
     }
+
+    /* Questions for interview */
+    /*  */
+    console.log('1');
+    const newPromise = new Promise((res, rej) => {
+      res(console.log('2'));
+    });
+    setTimeout(async () => {
+      await newPromise();
+    }, 5000);
+    console.log('3');
+  /*  */
+    let obj={a:1 , b : { a:2 , c:{ a:3 }}}
+    let obj1={...obj}
+    obj1.b=4
+    console.log(obj)
+    console.log(obj1)
+
     return(
         <div>
            <h1>TodosChild 1</h1>
